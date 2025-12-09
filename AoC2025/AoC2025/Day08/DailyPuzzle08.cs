5fb5efd9e09ee095a5d657971ea3f68bef57f0e8
Andy Smith <andy@rundevcycle.com>
2025-12-08 21:17:39 -0400

    Day 8, part 2.

--- AoC2025/AoC2025/Day08/DailyPuzzle08.cs
+++ AoC2025/AoC2025/Day08/DailyPuzzle08.cs
@@ -61,7 +61,42 @@ namespace AoC2025.Day08
 
         protected override void RunPart2()
         {
-            throw new NotImplementedException();
+            List<JBPoint> junctionBoxes = new();
+
+            // Convert all the lines into JBPoints.
+            foreach (var line in InputData)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+                junctionBoxes.Add(new JBPoint(line));
+            }
+
+            // Figure out all the potential string lights that would be needed to connect every box.
+            List<StringLight> stringLights = new();
+            for (int i = 0; i < junctionBoxes.Count; i++)
+            {
+                for (int j = i + 1; j < junctionBoxes.Count; j++)
+                {
+                    stringLights.Add(new StringLight(junctionBoxes[i], junctionBoxes[j]));
+                }
+            }
+
+            CircuitManager circManager = new CircuitManager();
+
+            // Display the shortest 10 connections.
+            foreach (var light in stringLights.OrderBy(l => l.Distance))
+            {
+                logger.Debug($"From {light.Box1} to {light.Box2} is {light.Distance}.");
+                circManager.AddStringLight(light);
+                if (circManager.Circuits.Count == 1 && circManager.Circuits.Single().JunctionBoxes.Count == junctionBoxes.Count)
+                {
+                    logger.Info($"Final circuits merged between {light.Box1} - {light.Box2}.");
+                    logger.Info($"Product of X coordinates is {light.Box1.X * light.Box2.X}.");
+                    break;
+                }
+            }
         }
     }
 }
